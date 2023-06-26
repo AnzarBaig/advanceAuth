da3a9b0a9f457e1811b28ecc96d81a176ee2bd69
Mirza Anzar Baig <mabaig222@gmail.com>
2023-06-26 21:54:24 +0530

    added all screens, few bug fixed

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Redirect, Route } from "react-router-dom";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   return (
